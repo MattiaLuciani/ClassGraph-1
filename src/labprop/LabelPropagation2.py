1843238ab7383e8b45b30e83ae1b5e4c6c0e6fc3
MattiaLuciani <mattia.luciani@gmail.com>
2021-11-03 16:59:33 +0100

    memory taken

--- src/labprop/LabelPropagation2.py
+++ src/labprop/LabelPropagation2.py
@@ -1,57 +1,64 @@
 from igraph import *
 from timeit import default_timer as timer
+import memory_profiler as mem_profile
 
-# First version of the propagation algorithm
 
 def lp1(max_iteration, data):
     
-    #file_uno = open("data.txt", "w")
-    #str1 = ' '.join(map(str, data))
-    #file_uno.write(str1)
-    #file_uno.close()
-    #print(data)
+    print('Memory (Before): ' + str(mem_profile.memory_usage()) + 'MB' )
     start = timer()
-    for v in range(max_iteration):
-        # Nodes at level i-1 send info to their neighbours at level i
-        tolabel_count = 0
-        
-        for i in range(len(data)):
-            if int(data[i][1] != 0) and len(data[i][2]) > 0:
-                for k in range(len(data[i][2])):
-                    # if the neighbour to be labelled don't have already a label
-                    to_label = int(data[i][2][k][0])
+    
+    for v in range(max_iteration): #for i = 1,2,...,#iter do
+
+        # Nodes at level i-1 send info to their neighbours at level i, populate level_1
+
+        for i in range(len(data)): #for all j ∈ Level(i−1) do
+
+            if data[i][1] != 0 and len(data[i][2]) > 0: #and data[i][1] > 1: #All labelled nodes with at least one egde
                 
-                    if data[to_label][1] == 0: # node to be labelled
-                        label = data[i][1]
-                        tl_weight = data[i][2][k][1]
-                        tmp = [tl_weight, label]
-                        
-                        #for i in range(len(tmp)):
-                            #tmp[i].sort(reverse=True)
-                        print(tmp)
-                        #data[to_label].append(tmp)
+                for k in range(len(data[i][2])): #for all edges e_jk do
+                    
+                    # if the neighbour to be labelled don't have already a label
+                    
+                    to_label = data[i][2][k][0] #Send to the node k the label lj with weight wjk
+                    
+                    if data[to_label][1] == 0:
+                        label = data[i][0] #error data[i][1]
+                        weight = data[i][2][k][1]
+                        tmp = [label, weight] 
+                        data[to_label].append(tmp) # Add node k to Level_i
+
+                data[i][2] = [] #Delete all the edges of node j
 
         # For each node at level i the final label is decided
-        
-        for i in range(len(data)): #50000
+
+        for i in range(len(data)):
+            
             len_line = len(data[i])
+            
             if len_line > 3:
                 possible_labels = []
                 for k in range(3, len_line):
                     possible_labels.append(data[i][k])
-                    print(possible_labels)
+                    
                 summing_list = []
+                
                 for j in range(len(possible_labels)):
-                    #print(possible_labels[j])
                     summing_list.append(possible_labels[j])
-                    if len(summing_list) > 0 and summing_list[len(summing_list) - 1][1] == possible_labels[j][1]:
+                    
+                    #print(len(summing_list) - 1][1])
+
+                    if summing_list[len(summing_list) - 1][1] == possible_labels[j][1]:
                         summing_list[len(summing_list) - 1][0] = summing_list[len(summing_list) - 1][0] + possible_labels[j][0]
+                        
                     else:
                         summing_list.append(possible_labels[j])
-                        #print("****************")
-                data[i][1] = summing_list[len(summing_list) - 1][1]
-               # print(data[i][1])
-                for k in range(3, len_line):
+                  
+                data[i][1] = summing_list[len(summing_list) - 1][1] #Level_i−1 = Level_i
+                
+                for k in range(3, len_line): #Level_i = ∅
                     del data[i][3]
+        
     end = timer()
-    #print("Time taken:", end-start)
+    print("Time taken:", end-start)
+    print('Memory (After) : ' + str(mem_profile.memory_usage()) + 'MB')
